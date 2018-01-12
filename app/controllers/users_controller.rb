51603e238d6b1f0d2cb396d1fbc32e6a3e21c016
Ryan Mahoney <ryan.mahoney@rtslabs.com>
2018-01-12 21:58:47 +0000

    Create account functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # user is saved to db
+    else
+      render 'new' #unable to save user, bring back the new user page
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, 
+                                :password, :password_confirmation)
+  end
+  
 end
